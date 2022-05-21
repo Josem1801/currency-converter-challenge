2fdd37e2a08d7efac0a58804b29eab68741d96e6
Josem1801 <Josemanuelrosales2@gmail.com>
2022-05-21 08:25:57 -0500

    Using SSR to get currency list

---
+++ src/pages/index.tsx
@@ -0,0 +1,35 @@
+import CurrencyConverter from 'components/CurrencyConverter'
+import { Layout } from 'Layout/styles'
+import type {
+  GetServerSideProps,
+  InferGetServerSidePropsType,
+  NextPage,
+} from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import { useEffect } from 'react'
+import getCurrenciesList, { Currency } from 'services/getCurrenciesList'
+
+export const getServerSideProps: GetServerSideProps<{
+  currencies: Currency
+}> = async () => {
+  const currencies = await getCurrenciesList()
+  return {
+    props: {
+      currencies,
+    },
+  }
+}
+
+const Home = ({
+  currencies,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  console.log(currencies)
+  return (
+    <Layout>
+      <CurrencyConverter currencies={currencies} />
+    </Layout>
+  )
+}
+
+export default Home
